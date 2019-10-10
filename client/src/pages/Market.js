499d04ea79dfe749d8b84db96195f2e8cf503e18
Your Name <you@example.com>
2019-10-10 11:43:56 -0500

    feature: creating base ui for login / signup pages and formatting code

--- client/src/pages/Market.js
+++ client/src/pages/Market.js
@@ -5,14 +5,14 @@ import BoothList from '../components/BoothList'
 function Market() {
     return(
         <section class="section is-small">
-        <div class="container">
-        <h1 class="title">Market Page</h1>
-        <h2 class="subtitle">
-            Search through farmers booths to discover goods in your area.
-        </h2>
-        <BoothList />
-        </div>
-    </section>
+            <div class="container">
+            <h1 class="title">Marketplace</h1>
+            <h2 class="subtitle">
+                Search through farmers booths to discover goods in your area.
+            </h2>
+            <BoothList />
+            </div>
+        </section>
     )
 }
 
