1ddb67b9943b7ed8a300e3f67e3cb517e21836ff
Ryan Jalufka <ryanjalufka@gmail.com>
2019-10-18 15:26:58 -0500

    chore: creating database

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 const app = express();
-
+import farmers from './routes/farmers'
 const PORT = 4000;
 
-app.listen(PORT, () =>
-  console.log(`App listening on port ${PORT}!`),
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/farmers', farmers);
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`)
+  require('./utils/db')
+  }
 );
 
 //to run server 'nodemon start.js'
