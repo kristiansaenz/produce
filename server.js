453ac88546e5523d6c3c2de74afcc4743a95a607
Ryan Jalufka <ryanjalufka@gmail.com>
2019-10-20 18:03:41 -0500

    chore: restructuring files for heroku deployment

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 app.use('/farmers', farmers);
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
---
+++ utils/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  LOCAL_MONGO_URI: 'mongodb://ryanjalufka:kijut123@ds331558.mlab.com:31558/produce'
+}
