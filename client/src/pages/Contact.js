13c343f45809c4aaeee726850c156af1c8bcdc04
Ryan Jalufka <ryanjalufka@gmail.com>
2019-10-20 14:09:53 -0500

    chore: saving Contact.js page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,16 +4,6 @@ import ContactForm from '../components/ContactForm'
 
 function Contact() {
     return ( 
-<<<<<<< HEAD
-        <section class = "section is-medium" >
-        <div class = "container" >
-        <h1 class = "title" > Contact Page </h1> 
-        <h2 class = "subtitle" >
-        4202 Weedlings Dr. < br / >
-        Windhelm, Einsedeln 28932 < br / >
-        +5(341) 432 - 2432 </h2> 
-        </div> 
-=======
         <section class = "section is-small">
             <div class = "container">
             <h1 class = "title">Contact</h1> 
@@ -36,7 +26,6 @@ function Contact() {
         </section>
 
 
->>>>>>> feature: creating base ui for login / signup pages and formatting code
         </section>
     )
 }
