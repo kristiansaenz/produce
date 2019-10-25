4780555cb69cd7009999c8de8d96b5514e3a0786
Your Name <you@example.com>
2019-10-25 15:16:12 -0500

    fix: changing boothpage

--- client/src/pages/BoothPage.js
+++ client/src/pages/BoothPage.js
@@ -4,6 +4,9 @@ import {
   Route,
   useParams
 } from 'react-router-dom'
+import BoothHeader from '../components/BoothHeader'
+import ItemList from '../components/ItemList'
+
 
 const BoothPage = () => {
 
@@ -21,11 +24,22 @@ const BoothPage = () => {
   }, []);
 
     return(
-      <div>
-        <ul>
-          <li>{boothInfo.name}</li>
-        </ul>
-      </div>
+
+      <section class = "section is-small">
+            <li>{boothInfo.name}</li>
+            <BoothHeader />
+            <br/>
+
+            <section class="hero">
+            <div class="hero-body">
+                {/* <h1 class="title">Your Items will appear here</h1>
+                <h2 class="subtitle">
+                    Buy goods here ~
+                </h2> */}
+                <ItemList />
+            </div>
+            </section>
+      </section>
     );
 }
 
