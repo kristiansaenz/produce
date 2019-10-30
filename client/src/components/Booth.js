524c54b2a32a7214693fcf98660f0ac1f6567898
Your Name <you@example.com>
2019-10-29 21:00:01 -0500

    adding location pin and fixing mobile margins

--- client/src/components/Booth.js
+++ client/src/components/Booth.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PinIcon from '../images/pin.svg'
 
 class Booth extends React.Component {
 
@@ -26,7 +27,7 @@ class Booth extends React.Component {
 
                     {/* Card Footer */}
                     <footer class="card-footer">
-                        <div class="card-footer-item">{this.props.address.city}, {this.props.address.state}</div>
+                        <div class="card-footer-item"><img src={PinIcon} className="pin" />{this.props.address.city}, {this.props.address.state}</div>
                     </footer>
                 </div>
                 </Link>
