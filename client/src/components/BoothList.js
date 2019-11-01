079e095b34a6bcb96c807c20e82688b9857f3e17
Ryan Jalufka <ryanjalufka@gmail.com>
2019-11-01 12:08:32 -0500

    chore: message when no booths are available

--- client/src/components/BoothList.js
+++ client/src/components/BoothList.js
@@ -5,22 +5,31 @@ import Booth from '../components/Booth'
 
 function BoothList(props){
 
-  return (
-    <div>
-      <div className="columns is-mobile">
-        {props.farmers.map(farmer => (
-          <Booth
-            id={farmer._id}
-            name={farmer.name}
-            email={farmer.email}
-            address={farmer.address}
-            booth_name={farmer.booth.booth_name}
-            booth_description={farmer.booth.description}
-          />
-        ))}
+  if (props.farmers.length === 0) {
+    return (
+      <div>
+        <h1>Hmm... There aren't any booths here :(</h1>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div>
+        <div className="columns is-mobile">
+
+          {props.farmers.map(farmer => (
+            <Booth
+              id={farmer._id}
+              name={farmer.name}
+              email={farmer.email}
+              address={farmer.address}
+              booth_name={farmer.booth.booth_name}
+              booth_description={farmer.booth.description}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default BoothList
