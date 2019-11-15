76710024fc36d4eef92046f409b51dbeea6f1797
Your Name <you@example.com>
2019-11-14 22:54:17 -0600

    feature: fixing ui

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,15 +8,36 @@ class Profile extends Component {
   render() {
     if(this.props.isAuthenticated) {
       return(
-        <div>
-          <div>
-            <img src={this.props.user.avatar} alt="avatar"/>
-            <h1>{this.props.user.name}</h1>
+
+        <section className="section is-small">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="columns">
+              <div className="form-titles">
+                <h1 className="title">Hello {this.props.user.name}</h1>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column is-one-third">
+                <br />
+                <img src={this.props.user.avatar} alt="avatar"/>
+                <ImageUploader user={this.props.user.email}/>
+              </div>
+            </div>
           </div>
-          <div>
-            <ImageUploader user={this.props.user.email}/>
-          </div>
-        </div>
+        </section>
+      </section>
+
+
+        // <div>
+        //   <div>
+        //     <img src={this.props.user.avatar} alt="avatar"/>
+        //     <h1>{this.props.user.name}</h1>
+        //   </div>
+        //   <div>
+        //     <ImageUploader user={this.props.user.email}/>
+        //   </div>
+        // </div>
       )
     } else {
     return(
