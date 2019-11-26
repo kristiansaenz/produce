69222cea5f868401e2ad1a2f8a549f46634b066b
Ryan Jalufka <ryanjalufka@gmail.com>
2019-11-26 12:00:00 -0600

    feature: creating EditBooth component

---
+++ client/src/components/EditBooth.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+
+function EditBooth(props) {
+
+  const [state, setState] = useState({
+    boothName:'',
+    description: ''
+  })
+
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log('submitting ', props.user.booth)
+    const boothUpdate = await axios.patch(`/booths/${props.user.booth}`, {
+      booth_name: state.boothName,
+      description: state.description
+    })
+    console.log(boothUpdate.data)
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Booth Name:
+          <input name="boothName" type="text" value={state.boothName} onChange={handleChange}/>
+        </label>
+        <label>
+          Description:
+          <textarea name="description"value={state.description} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Save Changes" />
+      </form>
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  user: state.auth.user
+})
+
+export default connect(mapStateToProps)(EditBooth)
---
+++ client/src/pages/ManageBooth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import EditBooth from '../components/EditBooth'
+
+class ManageBooth extends Component {
+  render() {
+    return(
+      <div>
+        <EditBooth />
+      </div>
+    )
+  }
+}
+
+export default ManageBooth;
